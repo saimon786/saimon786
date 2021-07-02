3b651593b711125ee7e1bc4f836f3b7bcf77f14a
saimon786 <nasaimonkhan@gmail.com>
2021-07-02 10:02:02 +0600

    Py_String

---
+++ Py-Learn/Python_Strings.py
@@ -0,0 +1,37 @@
+'''python string is an type of data . it writen with single or duble qutetion . '''
+
+# print('hello world')
+# print("hrllo world")
+
+# str in vriable 
+
+v ='saimon is a good boy !'
+
+# print(v)
+
+# print(v[1])
+
+# if 'is' not in v :
+#     print('yes')
+
+
+# else:
+#     print('good buy')
+
+
+# for i in v :
+#     print(i)
+
+
+
+# for i in "saimon is not bad boy !" :
+    # print(i)
+
+
+# print(len(v))
+
+
+
+''''python slicing '''
+'''python can return a slice of string . '''
+
